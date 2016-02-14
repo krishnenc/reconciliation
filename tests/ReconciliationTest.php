dd689a2d3fea1e01da5f2c971895d6c75dcc9c2d
krishnen <krishnenc@gmail.com>
2016-02-14 21:05:06 +0400

    Moved init of tests to constructor

--- tests/ReconciliationTest.php
+++ tests/ReconciliationTest.php
@@ -7,6 +7,21 @@ use App\ReconciliationUtils;
 
 class ReconciliationTest extends TestCase
 {
+
+    /*
+        Init criteria weight
+    */
+    function __construct() {
+       ReconciliationUtils::$maxdiffdate = 60;
+       ReconciliationUtils::$minconfidencescore = 50;
+       ReconciliationUtils::$amountweight = 0.4;
+       ReconciliationUtils::$walletrefweight = 0.3;
+       ReconciliationUtils::$dateweight = 0.15;
+       ReconciliationUtils::$idweight = 0.05;
+       ReconciliationUtils::$narrativeweight = 0.1;
+    }
+
+
     /**
      * Test that we can find an exact match for a transaction
      *
@@ -15,13 +30,6 @@ class ReconciliationTest extends TestCase
     public function testExactMatch()
     {
         $uploadPath = public_path().'/uploads';
-        ReconciliationUtils::$maxdiffdate = 60;
-        ReconciliationUtils::$minconfidencescore = 50;
-        ReconciliationUtils::$amountweight = 0.4;
-        ReconciliationUtils::$walletrefweight = 0.3;
-        ReconciliationUtils::$dateweight = 0.15;
-        ReconciliationUtils::$idweight = 0.05;
-        ReconciliationUtils::$narrativeweight = 0.1;
         $result = ReconciliationUtils::compareFile($uploadPath.'/test1File1.csv',$uploadPath.'/test1File2.csv');
         $this->assertEquals(1, $result['MATCHED']);
     }
@@ -34,13 +42,6 @@ class ReconciliationTest extends TestCase
     public function testFindSuggestion()
     {
         $uploadPath = public_path().'/uploads';
-        ReconciliationUtils::$maxdiffdate = 60;
-        ReconciliationUtils::$minconfidencescore = 50;
-        ReconciliationUtils::$amountweight = 0.4;
-        ReconciliationUtils::$walletrefweight = 0.3;
-        ReconciliationUtils::$dateweight = 0.15;
-        ReconciliationUtils::$idweight = 0.05;
-        ReconciliationUtils::$narrativeweight = 0.1;
         $result = ReconciliationUtils::compareFile($uploadPath.'/test2File1.csv',$uploadPath.'/test2File2.csv');
         $this->assertEquals(1, count($result['UNMATCHED_TRANSACTIIONS'][2]['SUGGESTIONS']));
     }
@@ -53,13 +54,6 @@ class ReconciliationTest extends TestCase
     public function testCannotFindSuggestion()
     {
         $uploadPath = public_path().'/uploads';
-        ReconciliationUtils::$maxdiffdate = 60;
-        ReconciliationUtils::$minconfidencescore = 50;
-        ReconciliationUtils::$amountweight = 0.4;
-        ReconciliationUtils::$walletrefweight = 0.3;
-        ReconciliationUtils::$dateweight = 0.15;
-        ReconciliationUtils::$idweight = 0.05;
-        ReconciliationUtils::$narrativeweight = 0.1;
         $result = ReconciliationUtils::compareFile($uploadPath.'/test3File1.csv',$uploadPath.'/test3File2.csv');
         $this->assertEquals(0, isset($result['UNMATCHED_TRANSACTIIONS'][2]['SUGGESTIONS']));
     }
